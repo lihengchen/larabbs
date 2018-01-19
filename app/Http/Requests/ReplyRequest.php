0335cc9c259fd7d7ee672723ea3599de0bbdfe71
luke2017 <l1270318090@gmail.com>
2018-01-19 03:58:31 +0000

    User can create reply

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -4,32 +4,11 @@ namespace App\Http\Requests;
 
 class ReplyRequest extends Request
 {
-    public function rules()
+ public function rules()
     {
-        switch($this->method())
-        {
-            // CREATE
-            case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
-            // UPDATE
-            case 'PUT':
-            case 'PATCH':
-            {
-                return [
-                    // UPDATE ROLES
-                ];
-            }
-            case 'GET':
-            case 'DELETE':
-            default:
-            {
-                return [];
-            };
-        }
+        return [
+            'content' => 'required|min:2',
+        ];
     }
 
     public function messages()
